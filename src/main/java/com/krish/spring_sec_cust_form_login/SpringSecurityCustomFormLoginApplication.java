0b94e0a5cfd9d8327c793fe866bafbeaef922bc4
kittulal <punnakrishnaa@gmail.com>
2026-01-04 09:15:08 +0530

    first commit

---
+++ src/main/java/com/krish/spring_sec_cust_form_login/SpringSecurityCustomFormLoginApplication.java
@@ -0,0 +1,13 @@
+package com.krish.spring_sec_cust_form_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCustomFormLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityCustomFormLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krish/spring_sec_cust_form_login/SpringSecurityCustomFormLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krish.spring_sec_cust_form_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityCustomFormLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
